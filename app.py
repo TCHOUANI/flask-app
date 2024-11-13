a14672e3b0e54992396204c0d28fe9c6ac5e747f
Tchouani Emmanuel <emmanueltchouani660@gmail.com>
2024-11-13 17:03:08 +0100

    Add Flask app with 500 error handling

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('/project_route')
-def home():
+@app.route('/')
+def index():
     return jsonify(message="Hello level 400 FET, Quality Assurance!")
-
-
+@app.route('/cause-error')
+def cause_error():
+    #Intentionally raise an exception to trigger a 500 error
+    raise Exception('An error occurred')
 if __name__ == '__main__':
     app.run(debug=True)
